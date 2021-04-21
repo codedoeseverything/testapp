a6f6546fd8d12e84d648159ee66b0bbc771bb66b
codedoeseverything <codedoeseverything@gmail.com>
2021-04-21 18:19:58 +1000

    15

--- env.sh
+++ env.sh
@@ -3,16 +3,19 @@
 export CUSTOMPLAIN_SKIPGLOBALLOGINFLAG=${CUSTOMPLAIN_SKIPGLOBALLOGINFLAG:-false}
 export CUSTOMPLAIN_PRDMODEFLAG=${CUSTOMPLAIN_PRDMODEFLAG:-true}
 
+env | grep CUSTOM
+
+echo "above env"
+
 while IFS='=' read -r name value ; do
     if [[ $name == 'CUSTOMPLAIN_'* ]]; then
-        sed -i "s#'<$name>'#${!name}#g" src/environments/environment.custom.ts
+        sed -i "s#'<$name>'#${!name}#g" environment.custom.ts
         echo "replacing <$name> to ${!name}"
     fi
 done < <(env)
 while IFS='=' read -r name value ; do
     if [[ $name == 'CUSTOM_'* ]]; then
-        sed -i "s#<$name>#${!name}#g" src/environments/environment.custom.ts
-        sed -i "s#<$name>#${!name}#g" angular.json
+        sed -i "s#<$name>#${!name}#g" environment.custom.ts
         echo "replacing <$name> to ${!name}"
     fi
 done < <(env)
